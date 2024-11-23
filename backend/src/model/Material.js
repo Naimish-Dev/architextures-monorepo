157391c5fcba8fb4e69f7d4802487162dc490477
chetanj99 <naimishbhesaniya212@gmail.com>
2024-11-23 15:51:06 +0530

    add material model

---
+++ backend/src/model/Material.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const materialSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
+  name: { type: String },
+  category: { type: String },
+  subcategory: { type: String, default: null },
+  description: { type: String },
+  collection: { type: String, default: "" },
+  thumbnail: { type: String },
+  realwidth: { type: Number, default: 0 },
+  realheight: { type: Number, default: 0 },
+  pixelwidth: { type: Number, default: 0 },
+  status: { type: String },
+  user: { type: mongoose.Schema.Types.Mixed, default: null },
+  brand: { type: mongoose.Schema.Types.Mixed, default: null },
+  country: { type: mongoose.Schema.Types.Mixed, default: null },
+  seamless: { type: String, default: "0" },
+  source_type: { type: String, default: "single" },
+  json_data: {
+    productUrl: { type: String, default: "" },
+    units: { type: String, default: "mm" },
+    appearance: {
+      tint: { type: String, default: "#FFFFFF" },
+      edgeStyle: { type: String, default: "" },
+      edgeScale: { type: String, default: "" },
+    },
+    appSettings: {
+      customSizes: { type: Boolean, default: false },
+      sizes: { type: Array, default: [] },
+      patterns: { type: Array, default: [] },
+      adjustments: { type: Boolean, default: false },
+      tint: { type: Boolean, default: false },
+      edges: { type: Boolean, default: false },
+      tone: { type: Boolean, default: false },
+    },
+  },
+  source_names: { type: [String], default: [] },
+  color: { type: String, default: "" },
+  link: { type: String, default: null },
+  source_data: { type: mongoose.Schema.Types.Mixed, default: null },
+  params: { type: mongoose.Schema.Types.Mixed, default: null },
+  patterns: { type: mongoose.Schema.Types.Mixed, default: null },
+  sku: { type: mongoose.Schema.Types.Mixed, default: null },
+  gallery_images: { type: [String], default: [] },
+  datasheet: { type: mongoose.Schema.Types.Mixed, default: null },
+  downloads: { type: Number, default: 0 },
+  texture_sources: { type: mongoose.Schema.Types.Mixed, default: null },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  deleted_at: { type: Date, default: null },
+  brands_name: { type: String, default: null },
+  brands_website_link: { type: String, default: null },
+  brands_logo: { type: String, default: null },
+  protextures_position_as_position: { type: Number, default: null },
+});
+
+const MaterialModel = mongoose.model("Material", materialSchema);
+
+module.exports = MaterialModel;
