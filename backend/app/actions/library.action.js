f34e92060256bc5e771a4c3fc60422c6fd158db4
Mahendra chavda <chavdamahendra247@gmail.com>
2024-12-08 13:10:51 +0530

    feat: texture save functionality implemented

--- backend/app/actions/library.action.js
+++ backend/app/actions/library.action.js
@@ -1,3 +1,22 @@
-export default function index(req, res, next) {
-  res.json({});
+import LibraryModel from "../models/library.model.js";
+
+export default async function index(req, res, next) {
+  try {
+    const body = req.body;
+    body.image.replace("data:image/png;base64,", "");
+
+    const payload = {
+      name: body.name,
+      params: body.params,
+      category: body.category,
+      user: req.user._id,
+      type: body.type,
+    };
+
+    const library = await LibraryModel.create(payload);
+
+    res.json({ id: library.id, status: "success" });
+  } catch (error) {
+    next(error);
+  }
 }
